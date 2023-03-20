091b2fc1ae688c294a0464f95cb15453c900634f
Alisher Dadametov <ksindao@gmail.com>
2023-03-20 17:08:46 +0300

    Во ViewController и storyboard добавлены лэйблы с текущим значением счётчика и кнопка с функцией увеличения значения счётчика на одну единицу при каждом её нажатии.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var counted: UILabel!
+    
+    
+    @IBAction func countButton(_ sender: Any) {
+        if let text = counted.text, let value = Int(text) {
+            counted.text = "\(value + 1)"
+            }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
